d7098005fd663e3808734828f48a0346d04e830c
Joshun <joshua.oleary@btinternet.com>
2015-10-10 15:15:49 +0100

    Further commenting

--- bags.hs
+++ bags.hs
@@ -22,16 +22,21 @@ module Bags where
   -- goes through list of tuples and puts duplicates togther - i.e. (5,1),(5,1),(3,1)(5,1) becomes (5,3),(3,1)
   listToBagCleanup :: Bag Int -> Bag Int
   listToBagCleanup [] = []
+  -- call listToBagCleanupA with the bag to clean up and an empty 'working' bag
   listToBagCleanup bag = listToBagCleanupA bag []
 
   -- takes a bag to clean up, a working bag and returns a new bag
+  -- the 'working' bag is a new bag, initially empty, used to build up the final bag which will have been stored more efficiently
   listToBagCleanupA bag workingBag
+    -- got to end of working bag, so just insert the item into it
     | null remainingBag = bagInsert nextItemType workingBag
+    -- otherwise insert item in place
     | otherwise = listToBagCleanupA remainingBag (bagInsert nextItemType workingBag)
     where ((nextItemType,nextItemCount):remainingBag) = bag
 
   itemCount :: ItemType -> Bag Int -> Int
   itemCount itemType bag
+    -- if the current item in list is of the type being queried, return its quantity
     | null remainingBag = if itemType == nextItemType then nextItemCount else 0
     | itemType == nextItemType = nextItemCount + (itemCount itemType remainingBag)
     | otherwise = itemCount itemType remainingBag
@@ -48,14 +53,19 @@ module Bags where
     | otherwise = ((nextItemType,nextItemCount):(bagInsert itemType remainingBag))
     where ((nextItemType,nextItemCount):remainingBag) = bag
 
+  -- sort both bags and then see if each (item,quantity) tuple is equal
   bagEqual :: Bag Int -> Bag Int -> Bool
+  -- two empty bags equal, but if just one of either is then they cannot be
   bagEqual [] [] = True
   bagEqual _ [] = False
   bagEqual [] _ = False
+  -- call bagEqualA on the sorted bags
   bagEqual bag1 bag2 = bagEqualA (sort bag1) (sort bag2)
 
   bagEqualA :: Bag Int -> Bag Int -> Bool
   bagEqualA (h1:t1) (h2:t2)
+    -- tails are null, so see if last elements are equal
     | null t1 && null t2 = h1 == h2
+    -- heads are equal, so see if rest of bag is
     | h1 == h2 = bagEqualA t1 t2
     | otherwise = False
