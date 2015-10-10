44e200b6eadcfdc003b0c39b9ad3a95a1df9cdce
Joshun <joshua.oleary@btinternet.com>
2015-10-10 13:22:16 +0100

    Initial commit

---
+++ bags.hs
@@ -0,0 +1,31 @@
+{- ********************
+   COM2001 Assignment 1
+   Bags
+   ******************** -}
+
+module Bags where
+  type ItemType = Int
+  type Item a = (a,Int)
+  -- data Item = (a,Int) deriving Eq (Eq,Int)
+  type Bag a = [Item a]
+
+  listToBag :: [ItemType] -> Bag Int
+  listToBag [] = []
+  listToBag (first:rest) = (first,1):listToBag rest
+
+  itemCount :: ItemType -> Bag Int -> Int
+  itemCount itemType bag
+    | null rest = if itemType == nextItemType then nextItemCount else 0
+    | itemType == nextItemType = nextItemCount + (itemCount itemType rest)
+    | otherwise = itemCount itemType rest
+    where ((nextItemType,nextItemCount):rest) = bag
+
+  bagInsert :: ItemType -> Bag Int-> Bag Int
+  bagInsert itemType bag
+    -- item doesn't exist, create new pair which counts 1 item of the type
+    | null remainingBag = (nextItemType,1):remainingBag
+    -- if item is already in list, create tuple with item type and count increased by 1 and cons it
+    | itemType == nextItemType = (nextItemType,nextItemCount+1):(remainingBag)
+    -- else keep going through bag seeing if item already exists
+    | otherwise = ((nextItemType,nextItemCount):(bagInsert itemType remainingBag))
+    where ((nextItemType,nextItemCount):remainingBag) = bag
